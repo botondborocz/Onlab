97622017b1f813ef25be4060ab06fd30cc2265fa
imre-gabor <gabor@aut.bme.hu>
2024-03-13 12:09:58 +0100

    add /api prefix

--- src/main/java/goalzone/controller/AverageUserController.java
+++ src/main/java/goalzone/controller/AverageUserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class AverageUserController {
 
